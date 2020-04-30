689f317291a6631ac7a37433c0c88b689371d8f8
YounesAz <younesazouzi5@gmail.com>
2020-04-30 02:17:25 +0000

    completed the backend for feature one.

--- onlinebookstore/src/main/java/ma/soft/unes/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/ma/soft/unes/onlinebookstore/config/RepositoryConfig.java
@@ -1,5 +1,23 @@
 package ma.soft.unes.onlinebookstore.config;
 
-public class RepositoryConfig {
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer{
+	
+	private EntityManager entityManager;
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		
+		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+				.map(Type::getJavaType)
+				.toArray(Class[]::new));
+		
+	}
 
 }
